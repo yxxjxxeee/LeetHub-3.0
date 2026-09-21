0d87a750abf4669e68b5c6b190b616b5c05d9cfb
YJE <c0d1ngyje@gmail.com>
2026-09-21 10:17:29 +0900

    Fix: apply base path to all GitHub upload paths

--- src/js/leetcode.js
+++ src/js/leetcode.js
@@ -117,14 +117,14 @@ function constructGitHubPath(
     console.log('Language:', language);
     if (language) {
       const path = useDifficultyFolder
-        ? `${language}/${difficulty}/${filePath}`
-        : `${language}/${filePath}`;
+        ? `${basePath}/${language}/${difficulty}/${filePath}`
+        : `${basePath}/${language}/${filePath}`;
       return `https://api.github.com/repos/${hook}/contents/${path}`;
     }
   }
   const path = useDifficultyFolder
     ? `${basePath}/${difficulty}/${filePath}`
-    : `${filePath}`;
+    : `${basePath}/${filePath}`;
   return `https://api.github.com/repos/${hook}/contents/${path}`;
 }
 
@@ -1635,8 +1635,8 @@ async function appendProblemToReadme(topic, markdownFile, hook, problem) {
     console.log('Language:', language);
     if (language) {
       path = useDifficultyFolder
-        ? `${language}/${difficulty}/${filePath}`
-        : `${language}/${filePath}`;
+        ? `${basePath}/${language}/${difficulty}/${filePath}`
+        : `${basePath}/${language}/${filePath}`;
     } else {
       console.log("No language found for problem:", problem);
       return ''
@@ -1644,7 +1644,7 @@ async function appendProblemToReadme(topic, markdownFile, hook, problem) {
   } else {
     path = useDifficultyFolder
     ? `${basePath}/${difficulty}/${filePath}`
-    : `${filePath}`;
+    : `${basePath}/${filePath}`;
   }
 
   const url = `https://github.com/${hook}/tree/main/${path}`;
